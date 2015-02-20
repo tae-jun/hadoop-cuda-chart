f0728f551db80c47a4de68f7284b98d9ad08ce6b
tae-jun <i2r.jun@gmail.com>
2015-02-20 15:10:22 +0900

    IDE changed: web storm --> atom & ignore css

--- web/scripts/_references.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="typings/jquery/jquery.d.ts" />
-/// <reference path="typings/highcharts/highcharts.d.ts" />
-/// <reference path="typings/angularjs/angular.d.ts" />
-/// <reference path="typings/angularjs/angular-animate.d.ts" />
-/// <reference path="typings/angularjs/angular-route.d.ts" />
