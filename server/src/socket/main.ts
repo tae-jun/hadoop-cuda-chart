de8838246b69cba8d8e6bbe7cf95d9c304e378eb
tae-jun <i2r.jun@gmail.com>
2015-03-04 19:00:04 +0900

    server socket update

--- server/src/socket/main.ts
+++ server/src/socket/main.ts
@@ -1,13 +1,44 @@
-import db = require('../mongo/hcc/hdLog/main');
-
 var io:SocketIO.Server = require('../app').io;
 
-// On socket.io connected
-io.on('connection', (socket)=> {
-    console.log('+++ New incoming socket.io connection');
+var socket:SocketIO.Socket;
 
-    socket.emit('api', '/ws/v1/history', (data)=> {
+// On socket.io connected
+io.on('connection', (_socket)=> {
+    console.log('+++ New incoming socket.io connection');
+    socket = _socket;
+
+    socket.on('error', (err)=> {
+        console.error('--- Socket connection error');
+        console.error(err);
+        socket = null;
+    });
+
+    socket.on('disconnect', ()=> {
+        console.log('=== Socket disconnected');
+        socket = null;
+    });
+
+
+    /**
+     * TODO:
+     * DELETE HERE
+     */
+    request('/ws/v1/history', (err, data)=> {
+        console.log('+++ Got history info');
         console.log(data);
     });
 });
 
+/**
+ *
+ * @param url example: /ws/v1/history
+ * @param callback
+ */
+export function request(url:string, callback:(err, data)=>void) {
+    if (socket == null)
+        return callback(new Error('Socket is closed'), null);
+
+    socket.emit('api', url, (data)=> {
+        callback(null, data);
+    });
+}
