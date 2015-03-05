e985d11b795eb13ca47445916e0194c9e2f1ac5f
tae-jun <i2r.jun@gmail.com>
2015-03-05 13:18:50 +0900

    Debugging...

--- server/src/route/history/index.ts
+++ server/src/route/history/index.ts
@@ -7,6 +7,10 @@ router
 
     .all('/*', (req, res, next)=> {
         api.request('/ws/v1/history/' + req.params[0], (err, data)=> {
+            if (err)
+                console.error(err);
+            console.log(typeof data);
+            console.log(data);
             res.json(new Form(err, data));
         });
     });
