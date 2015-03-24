ff44decb101bbe7ebe3931d5a3c16b0a1792b479
tae-jun <i2r.jun@gmail.com>
2015-03-24 17:31:26 +0900

    working

--- web/src/nav/directives/plJobButton.ts
+++ web/src/nav/directives/plJobButton.ts
@@ -11,12 +11,12 @@ module nav {
             templateUrl: 'tpl/plJobButton.tpl.html',
             replace: true,
             link: (scope, elem, attr) => {
-                console.log(scope['job']);
-
                 scope.$on('$locationChangeSuccess', (event, newState, oldState)=> {
                     console.log('newState', newState);
                     console.log('oldState', oldState);
                     console.log('route current params', $route.current.params);
+                    var jobId = $route.current.params['jobId'];
+
                 });
             }
         }
