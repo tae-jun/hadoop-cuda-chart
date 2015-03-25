5f08e25da6d6341f8f27deebd8aa3b109edb4595
tae-jun <i2r.jun@gmail.com>
2015-03-25 16:53:58 +0900

    working

--- web/src/chart/controllers/ChartCtrl.ts
+++ web/src/chart/controllers/ChartCtrl.ts
@@ -6,37 +6,32 @@ module chart {
     export class ChartCtrl {
 
         constructor($scope:IChartScope,
-                    $mdSidenav,
                     chartService:ChartService,
                     job,
                     toolbarService:toolbar.ToolbarService,
                     $routeParams) {
-            $scope.toggleSidenav = (menuId) => {
-                $mdSidenav(menuId).toggle();
-            };
             var jobId = $routeParams.jobId;
             var nodeName = $routeParams.nodeName;
 
-            toolbarService.setTitle('Chart', jobId, nodeName);
+            // Set title
+            if (nodeName)
+                toolbarService.setTitle('Chart', jobId, nodeName);
+            else
+                toolbarService.setTitle('Chart', jobId);
 
-            console.log(job);
+
 
             var tasks = job.tasks;
             var nodeTasks = job.nodeTasks;
 
-            console.log('ChartCtrl', 'tasks', tasks);
+            console.log('ChartCtrl', 'job', job);
             chartService.setChart(tasks);
-            console.log(tasks);
-            console.log(nodeTasks);
 
             $scope.nodes = nodeTasks;
-
-
         }
     }
 
     export interface IChartScope extends ng.IScope {
-        toggleSidenav(menuId);
         chartConfig:any;
         nodes:Object;
     }
